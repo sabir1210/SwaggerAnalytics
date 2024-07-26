d62e27bda28a1ce5feeb27c6fa61bc7d0bddf99c
SabirHussain <sabirhussainkhaaki@gmail.com>
2024-07-26 17:09:39 +0500

    api call push

---
+++ SwaggerAnalytics/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+
+namespace SwaggerAnalytics.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("GetRealTimeUsers")]
+        public IActionResult GetActiveUsers()
+        {
+            dynamic response = new ExpandoObject();
+            var activeUsers = ActiveUsersService.ActiveUsers;
+            response.count = activeUsers.Count();
+            response.activeUsers = activeUsers;
+            return Ok(response);
+        }
+    }
+}
